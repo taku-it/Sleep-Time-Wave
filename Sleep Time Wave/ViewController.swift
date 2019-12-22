45ad075adb6f5128f526f8afec159e0c1dfc1871
Ikuta Takuto <o-taku0124@ezweb.ne.jp>
2019-12-22 11:11:48 +0900

    Initial Commit

---
+++ Sleep Time Wave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sleep Time Wave
+//
+//  Created by 大岡拓登 on 2019/12/22.
+//  Copyright © 2019 IkutaTakuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sleep Time WaveTests/Sleep_Time_WaveTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sleep_Time_WaveTests.swift
+//  Sleep Time WaveTests
+//
+//  Created by 大岡拓登 on 2019/12/22.
+//  Copyright © 2019 IkutaTakuto. All rights reserved.
+//
+
+import XCTest
+@testable import Sleep_Time_Wave
+
+class Sleep_Time_WaveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
